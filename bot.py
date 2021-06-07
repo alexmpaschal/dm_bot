32974eb8a1299ed90ac4649cf6b5fe17a681da3a
yaboiwierdal <78000452+yaboiwierdal@users.noreply.github.com>
2021-06-06 23:28:09 -0400

    Fixed formatting and removed comments.

--- bot.py
+++ bot.py
@@ -11,9 +11,7 @@ GUILD = "GUILD"
 
 bot = commands.Bot(
     command_prefix=">",
-    activity=discord.Activity(
-        type=discord.ActivityType.listening, name=">help"
-    ),
+    activity=discord.Activity(type=discord.ActivityType.listening, name=">help"),
 )
 
 
@@ -27,7 +25,7 @@ async def on_ready():
     for guild in bot.guilds:
         if guild.name == GUILD:
             break
-    
+
     print(
         f"{bot.user} is connected to the following guild:\n"
         f"name: {guild.name}, id: {guild.id}"
@@ -53,35 +51,37 @@ class DM_settings(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def dm_add(
-        self, ctx, *, command
-    ):
+    async def dm_add(self, ctx, *, command):
         """Add command to monitor"""
         user = ctx.author
         user = user.id
 
-        find_command = cursor.execute(f"""SELECT command, enabled FROM user_commands WHERE command = (?) AND discord_user = (?)""", (command, user)).fetchone()
+        find_command = cursor.execute(
+            f"""SELECT command, enabled FROM user_commands WHERE command = (?) AND discord_user = (?)""",
+            (command, user),
+        ).fetchone()
 
         print(find_command[1])
 
-        if find_command is not None:    
-            #check enabled value
+        if find_command is not None:
+            # check enabled value
             if find_command[1] == 1:
                 return await ctx.send("You already have that command enabled.")
-                
+
             return await toggle_on(ctx, user, command)
 
         return await add_command(ctx, user, command)
 
     @commands.command()
-    async def dm_remove(
-        self, ctx, *, command
-    ):
+    async def dm_remove(self, ctx, *, command):
         """Remove monitored command"""
         user = ctx.author
         user = user.id
 
-        find_command = cursor.execute(f"""SELECT command, enabled FROM user_commands WHERE command = (?) AND discord_user = (?)""", (command, user)).fetchone()
+        find_command = cursor.execute(
+            f"""SELECT command, enabled FROM user_commands WHERE command = (?) AND discord_user = (?)""",
+            (command, user),
+        ).fetchone()
 
         if find_command is not None:
             if find_command[1] == 0:
@@ -94,7 +94,8 @@ class DM_settings(commands.Cog):
 
 async def add_command(ctx, user, command):
     cursor.execute(
-        f"""INSERT INTO user_commands (discord_user, command, enabled) VALUES (?, ?, ?)""", (user, command, 1)
+        f"""INSERT INTO user_commands (discord_user, command, enabled) VALUES (?, ?, ?)""",
+        (user, command, 1),
     )
     connection.commit()
 
@@ -103,7 +104,8 @@ async def add_command(ctx, user, command):
 
 async def toggle_on(ctx, user, command):
     cursor.execute(
-        """ UPDATE user_commands SET enabled = (?) WHERE discord_user = (?) AND command = (?)""", (1, user, command)
+        """ UPDATE user_commands SET enabled = (?) WHERE discord_user = (?) AND command = (?)""",
+        (1, user, command),
     )
     connection.commit()
 
@@ -112,7 +114,8 @@ async def toggle_on(ctx, user, command):
 
 async def toggle_off(ctx, user, command):
     cursor.execute(
-        """ UPDATE user_commands SET enabled = (?) WHERE discord_user = (?) AND command = (?)""", (0, user, command)
+        """ UPDATE user_commands SET enabled = (?) WHERE discord_user = (?) AND command = (?)""",
+        (0, user, command),
     )
     connection.commit()
 
@@ -126,14 +129,7 @@ def setup(bot):
 setup(bot)
 
 
-#channel.history(after=message) use for channel
-
-#asyncio.sleep() use for pausing script
-
-#Add message.author.user functionality into logic for loop
-
-
-@bot.listen('on_message')
+@bot.listen("on_message")
 async def message_listener(message):
     guild = message.guild
     link = message.jump_url
@@ -149,7 +145,7 @@ async def message_listener(message):
                     embed = await dm_if_link(message, command, bot_message)
                     return await message.author.send(embed=embed)
 
-                embed =  await dm_if_embed(message, command)
+                embed = await dm_if_embed(message, command)
                 return await message.author.send(embed=embed)
 
             embed = await dm_if_not_embed(message, command, bot_message)
@@ -160,9 +156,9 @@ async def wait_for_func(message):
     channel = message.channel
 
     def check(m):
-        return m.author.bot and m.channel==channel
+        return m.author.bot and m.channel == channel
 
-    msg = await bot.wait_for('message', check=check)
+    msg = await bot.wait_for("message", check=check)
 
     return msg
 
@@ -173,14 +169,16 @@ async def is_user_command(message):
     message_start = message.content.split(" ")[0]
 
     user_commands = cursor.execute(
-        """SELECT command FROM user_commands WHERE discord_user = (?) AND enabled = (?)""", (user, 1)
+        """SELECT command FROM user_commands WHERE discord_user = (?) AND enabled = (?)""",
+        (user, 1),
     ).fetchall()
 
     for command in user_commands:
         if message_start == command[0]:
             return command, True
-        
+
     return " ", False
+
 
 async def dm_if_link(message, command, bot_message):
     guild = message.guild
@@ -202,7 +200,6 @@ async def dm_if_embed(message, command):
     return embed
 
 
-
 async def dm_if_not_embed(message, command, bot_message):
     guild = message.guild
     link = message.jump_url
@@ -219,11 +216,19 @@ async def dm_if_not_embed(message, command, bot_message):
 
 
 async def create_alert_embed(command, guild, message, value, link):
-    embed = discord.Embed(title='Command Alert', description=f'You just used `{command[0]}` in {guild.name}', color=0x0000)
-    embed.set_thumbnail(url="https://raw.githubusercontent.com/yaboiwierdal/dm_bot/main/images/circle-cropped(4).png")
+    embed = discord.Embed(
+        title="Command Alert",
+        description=f"You just used `{command[0]}` in {guild.name}",
+        color=0x0000,
+    )
+    embed.set_thumbnail(
+        url="https://raw.githubusercontent.com/yaboiwierdal/dm_bot/main/images/circle-cropped(4).png"
+    )
     embed.add_field(name="Message", value=f"{message.content}", inline=False)
     embed.add_field(name="Bot's Reply", value=f"{value}", inline=False)
-    embed.add_field(name="Conversation", value=f"[Jump to message!]({link})", inline=False)
+    embed.add_field(
+        name="Conversation", value=f"[Jump to message!]({link})", inline=False
+    )
     embed.timestamp = datetime.datetime.utcnow()
 
     return embed
